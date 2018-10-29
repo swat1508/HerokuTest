4a531544f51c22f7b457d6e5115b3806a0814bd3
unknown <swatantrasinha1508@gmail.com>
2018-10-29 12:56:27 +0530

    pushing 1

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World! from Swatantra Sinha , this is working well yo yo yo !!!'));
 
+
+app.use(express.static('public'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
